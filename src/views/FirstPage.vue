76fbcc4e2000a3687d8acc6443f9ff25b2157b32
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2021-01-22 17:28:49 +0100

    onboarding in progress

---
+++ src/views/FirstPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg text-center">
+    <div class="card card-style">
+         <h5 class="pt-5" style="font-weight: 500; width: '205px;">Verify your account</h5>
+         <p class="mx-auto" style="width: 489px;">A statement to inform the user why their account needs to be verified before they can upload items on the platform</p>   
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style scoped>
+.card-style{
+    text-align: center;
+    margin: auto;
+    width: 860px;
+    height: 796px;
+    margin-top: 70px;
+    background: #FFFFFF;
+    border-radius: 10px;
+}
+</style>
---
+++ src/views/SecondPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Page 2</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
